2f775b0a6e37539234125e9242da2599395c8ffe
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2024-03-26 21:47:40 +0200

    restructuring of the project

--- src/pages/SeaBattle/SeaBattle.jsx
+++ src/pages/SeaBattle/SeaBattle.jsx
@@ -1,49 +1,14 @@
 import React from 'react';
 
-import Ship from './Ships/Ship';
+import Field from './Fields/Field';
+import Ships from './Ships/Ships';
 import './styles.css';
 
 const SeaBattle = () => {
     return (
         <div className="seabattle__container">
-            <div className="seabattle__container_ships">
-                <Ship className="seabattle__container_ship_four" />
-                <Ship className="seabattle__container_ship_three" />
-                <Ship className="seabattle__container_ship_three" />
-                <Ship className="seabattle__container_ship_two" />
-                <Ship className="seabattle__container_ship_two" />
-                <Ship className="seabattle__container_ship_two" />
-                <Ship className="seabattle__container_ship_one" />
-                <Ship className="seabattle__container_ship_one" />
-                <Ship className="seabattle__container_ship_one" />
-                <Ship className="seabattle__container_ship_one" />
-            </div>
-            <div className="seabattle__wrapper">
-                <div className="seabattle__wrapper_left_field">
-                    {Array(10 * 10)
-                        .fill()
-                        .map((el, index) => (
-                            <div
-                                className="seabattle__wrapper_cell"
-                                key={index}
-                            >
-                                {index}
-                            </div>
-                        ))}
-                </div>
-                <div className="seabattle__wrapper_right_field">
-                    {Array(10 * 10)
-                        .fill()
-                        .map((el, index) => (
-                            <div
-                                className="seabattle__wrapper_cell"
-                                key={index}
-                            >
-                                {index}
-                            </div>
-                        ))}
-                </div>
-            </div>
+            <Ships />
+            <Field />
         </div>
     );
 };
---
+++ src/pages/SeaBattle/Ships/Ships.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Ship from './Ship/Ship';
+import './styles.css';
+
+const Ships = () => {
+    return (
+        <div className="seabattle__container_ships">
+            <Ship ship={'four'} />
+            <Ship ship={'three'} />
+            <Ship ship={'three'} />
+            <Ship ship={'two'} />
+            <Ship ship={'two'} />
+            <Ship ship={'two'} />
+            <Ship ship={'one'} />
+            <Ship ship={'one'} />
+            <Ship ship={'one'} />
+            <Ship ship={'one'} />
+        </div>
+    );
+};
+
+export default Ships;
