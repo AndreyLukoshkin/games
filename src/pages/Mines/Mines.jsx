a0328d16eb529a936742b03fde009edd34b97877
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2024-04-09 21:24:03 +0300

    add top side, mines

--- src/pages/Mines/Mines.jsx
+++ src/pages/Mines/Mines.jsx
@@ -9,23 +9,30 @@ const Mines = () => {
         return;
     };
     return (
-        <div className="mines__container">
+        <div className="mines__wrapper">
             <ModalWindowRulesStart
                 mainRules={rulesMinesGame.mainRules}
                 textRules={rulesMinesGame.textRules}
                 start={start}
             />
-            <div className="mines__container_field">
-                {Array(10 * 10)
-                    .fill()
-                    .map((el, index) => (
-                        <div
-                            className="mines__container_field_cell"
-                            key={index}
-                        >
-                            {index}
-                        </div>
-                    ))}
+            <div className="mines__container">
+                <div className="mines__container_top">
+                    <div className="mines__container_top_score"></div>
+                    <div className="mines__container_top_smile"></div>
+                    <div className="mines__container_top_time"></div>
+                </div>
+                <div className="mines__container_field">
+                    {Array(10 * 10)
+                        .fill()
+                        .map((el, index) => (
+                            <div
+                                className="mines__container_field_cell"
+                                key={index}
+                            >
+                                {index}
+                            </div>
+                        ))}
+                </div>
             </div>
         </div>
     );
